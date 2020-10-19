4d2d4af23210ad368b75120e866069101dee53a1
aandilolo <61253019+aandilolo@users.noreply.github.com>
2020-10-19 17:26:50 -0500

    Update APIService.js

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://aandilolo.pythonanywhere.com'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'https://movies-groyce.pythonanywhere.com/'*/
+const API_URL = 'https://movies-groyce.pythonanywhere.com'; /* 'http://aandilolo.pythonanywhere.com/' 'http://localhost:8000' http://127.0.0.1:8000/ or  'https://movies-groyce.pythonanywhere.com/'*/
 
 export class APIService {
   constructor() {
